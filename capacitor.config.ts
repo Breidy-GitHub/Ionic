6fd95b2cbc530ca8bb59add0641a48942cc4d60d
NOBODY <nobodyspecial_@outlook.com>
2023-06-16 10:15:32 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Jonathan',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
